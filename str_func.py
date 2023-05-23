481070fa8abe3f4bb08937ac2b205106cd4b804e
Persiteo <Persiteo@users.noreply.github.com>
2023-05-24 01:02:36 +0300

    add changes to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def text_convert(text):
+    """
+    Converting text 
+    :return: Converted text
+    """
     return text.capitalize()
+
+
+def letters_converted(words):
+        """
+        делает заглавными первые буквы каждого слова в строке
+        """
+        print(words.title())
+
+
+letters_converted(words=input())
